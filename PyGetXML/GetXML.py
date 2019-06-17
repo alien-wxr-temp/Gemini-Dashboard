71c4a9ce8e9d652e8efe22e2862916ff07e45256
alien-wxr-temp <49577496+alien-wxr-temp@users.noreply.github.com>
2019-06-17 17:44:12 +0800

    Revert "update"
    This reverts commit b4b410865845ed10967591e9962125c8d4d3fd2a.

--- PyGetXML/GetXML.py
+++ PyGetXML/GetXML.py
@@ -1,29 +1,16 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-import time
 
 url ='https://us-aus-cuic1:8444/cuicui/permalink/?viewId=65B8C92C10000165005C5FD87D5BEAE0&linkType=xmlType&viewType=Grid'
+myXML = requests.get(url, verify=False)
+soup = BeautifulSoup(myXML.text, "lxml")
 
-import time,os
-
-def getXML(inc = 60):
-
-  while True:
-    myXML = requests.get(url, verify=False)
-    ticks=str(int(time.time()))
-    soup = BeautifulSoup(myXML.text, "lxml")
-
-    with open('C:/Users/eriwang/OneDrive - National Instruments/电话Dashboard项目/XMLData/'+ticks+'.txt', 'w') as f:
-        for row_tag in soup.report.children:
-            f.write('No.'+row_tag['index']+'\n')
-            for col_tag in row_tag.children:
-                f.write(col_tag['index']+' '+col_tag['name']+' ')
-                if (len(col_tag.contents) != 0):
-                    f.write(col_tag.contents[0]+'\n')
-                else:
-                    f.write('NULL'+'\n')
-
-    time.sleep(inc)
-
-getXML(1)
+for row_tag in soup.report.children:
+    print('No.'+row_tag['index'])
+    for col_tag in row_tag.children:
+            print(col_tag['index']+' '+col_tag['name'],end=' ')
+            if (len(col_tag.contents) != 0):
+                print(col_tag.contents[0])
+            else:
+                print('NULL')
